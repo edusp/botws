abe1841f377c38bc4094717255aa416f71ad0378
edpereira <edpereira@ebay.com>
2019-06-24 17:26:45 +1000

    Changed Response type

--- src/main/java/com/bot/ws/BotWebserviceController.java
+++ src/main/java/com/bot/ws/BotWebserviceController.java
@@ -1,17 +1,26 @@
 package com.bot.ws;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/botws")
 public class BotWebserviceController {
 	
 	@GetMapping("/{name}")
-	public String hello(@PathVariable("name") String name) {
-		return "Hello " + name;
+	public ResponseEntity hello(@PathVariable("name") String name) {
+
+		Map<String, String> map = new HashMap<>();
+
+		map.put("name", name);
+
+		return ResponseEntity.ok(map);
 	}
 
 }
