f9e9cde7d5872912d73101762457371eec64e7f6
edpereira <edpereira@ebay.com>
2019-06-27 11:43:01 +1000

    Find customer by ID

--- src/main/java/com/bot/ws/controllers/CustomerController.java
+++ src/main/java/com/bot/ws/controllers/CustomerController.java
@@ -4,10 +4,9 @@ import com.bot.ws.model.Customer;
 import com.bot.ws.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "customers")
@@ -23,5 +22,19 @@ public class CustomerController {
         Customer savedCustomer = customerRepository.save(customer);
 
         return ResponseEntity.ok(savedCustomer);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getUser(@PathVariable(name = "id") long id){
+
+        Customer customer = customerRepository.findOne(id);
+
+       if (customer != null) {
+        return ResponseEntity.ok(customer);
+
+       }
+
+       return ResponseEntity.notFound().build();
+
     }
 }
